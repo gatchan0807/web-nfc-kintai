8063bd99644f1c95f5c1266780aa4d063d78d0d0
がっちゃん <yuki.dolphin.koga@gmail.com>
2020-12-06 01:22:55 +0900

    💬 ステータスメッセージ用のリストを追加

---
+++ src/constants/message.js
@@ -0,0 +1,7 @@
+export const MAIN_MESSAGE = {
+  READ_WAIT: "Read Wait...",
+  SENDING: "Sending...",
+  SUCCESS: "Success!",
+  NON_SUPPORT: "Not Supported.",
+  WRITE_STAND_BY: "Write Stand by...",
+};
