5c0804c1186ad3b454f8e859ba4a3fdd6a9be3aa
がっちゃん <yuki.dolphin.koga@gmail.com>
2020-12-06 01:30:17 +0900

    ✨ NFC対応情報を元にステータスメッセージを切り替えるロジックを追加

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../../styles/components/Status.module.css";
 import { MAIN_MESSAGE } from "../constants/message";
 
 export default function Status() {
-  const [statusMessage, setStatusMessage] = useState(MAIN_MESSAGE.READ_WAIT);
+  const [statusMessage, setStatusMessage] = useState(MAIN_MESSAGE.NON_SUPPORT);
+
+  useEffect(() => {
+    if (process.browser) {
+      if ("NDEFReader" in window && "NDEFWriter" in window) {
+        setStatusMessage(MAIN_MESSAGE.READ_WAIT);
+      }
+    }
+  });
 
   return <div className={styles.circle}>{statusMessage}</div>;
 }
