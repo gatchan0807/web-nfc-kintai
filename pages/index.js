9e48048d39ca3f36fbac24f71441c2d8cd84624a
がっちゃん <yuki.dolphin.koga@gmail.com>
2020-12-06 13:18:53 +0900

    ✨ パスコード登録画面のマークアップ

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>WebNFCで勤怠送るくん！</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
--- pages/register.js
+++ pages/register.js
@@ -1,16 +1,47 @@
+import Head from "next/head";
 import Link from "next/link";
+
 import styles from "../styles/Home.module.css";
+import Status from "../src/components/status";
 
 export default function Register() {
   return (
     <div className={styles.container}>
-      <h1>はじめまして！</h1>
-      <div>
-        <p>Hello World!</p>
-        <p>
-          <Link href="/">TOPに戻る</Link>
-        </p>
-      </div>
+      <Head>
+        <title>パスコード登録</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1>カードにパスコードを登録</h1>
+        <div>
+          <div>
+            <div>
+              <h2>カードIDを確認</h2>
+              <p>
+                まずはNFCカードのIDを読み込んで確認し、スプレッドシートに登録してください
+              </p>
+              <label>カードID</label>
+              <input placeholder="NFCカードID" />
+            </div>
+            <div>
+              <h2>カードに登録するパスコードを入力</h2>
+              <p>
+                NFCカードにパスコードを書き込み、NFCカードIDと紐付けてスプレッドシートに登録してください
+              </p>
+              <label>パスコード</label>
+              <input placeholder="パスコード" type="password" />
+            </div>
+            <Status></Status>
+            <div>
+              <label>書き込みチェック</label>
+              <button>チェック開始</button>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <Link href="/">TOPに戻る</Link>
+      </footer>
     </div>
   );
 }
