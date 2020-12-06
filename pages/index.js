bc4f23846d8debc4d60621ea52b5fa5fa357eb2e
がっちゃん <yuki.dolphin.koga@gmail.com>
2020-12-06 10:28:05 +0900

    💄 リンクのUI調整

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
@@ -14,35 +13,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          カードにパスコードを<Link href="/register">登録する</Link>
-        </h1>
-        <NFCUsableFlag></NFCUsableFlag>
+        <h1 className={styles.title}>WebNFCで！勤怠送るくん！</h1>
         <Status></Status>
+        <span className={styles.link}>
+          <Link href="/register">カードにパスコードを登録する</Link>
+        </span>
       </main>
       <footer className={styles.footer}>このアプリについて</footer>
-    </div>
-  );
-}
-
-function NFCUsableFlag() {
-  const [nfcReadFlag, setNFCReadFlag] = useState("False");
-  const [nfcWriteFlag, setNFCWriteFlag] = useState("False");
-
-  useEffect(() => {
-    if (process.browser) {
-      if ("NDEFReader" in window) {
-        setNFCReadFlag("True");
-      }
-      if ("NDEFWriter" in window) {
-        setNFCWriteFlag("True");
-      }
-    }
-  });
-
-  return (
-    <div>
-      NFC Usable (Read): {nfcReadFlag} / NFC Usable (Write): {nfcWriteFlag}
     </div>
   );
 }
