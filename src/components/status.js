8a2071fbeca26630bf02436eb3802148a6228577
がっちゃん <yuki.dolphin.koga@gmail.com>
2020-12-06 18:27:28 +0900

    ♻ 読み込みステータス表示コンポーネントでPropsを読むように変更

--- src/components/status.js
+++ src/components/status.js
@@ -1,17 +1,5 @@
-import { useState, useEffect } from "react";
 import styles from "../../styles/components/Status.module.css";
-import { MAIN_MESSAGE } from "../constants/message";
 
-export default function Status() {
-  const [statusMessage, setStatusMessage] = useState(MAIN_MESSAGE.NON_SUPPORT);
-
-  useEffect(() => {
-    if (process.browser) {
-      if ("NDEFReader" in window && "NDEFWriter" in window) {
-        setStatusMessage(MAIN_MESSAGE.READ_WAIT);
-      }
-    }
-  });
-
-  return <div className={styles.circle}>{statusMessage}</div>;
+export default function Status(props) {
+  return <div className={styles.circle}>{props.message}</div>;
 }
--- src/constants/message.js
+++ src/constants/message.js
@@ -2,6 +2,7 @@ export const MAIN_MESSAGE = {
   READ_WAIT: "Read Wait...",
   SENDING: "Sending...",
   SUCCESS: "Success!",
+  INPUT_WAIT: "Input Wait...",
   NON_SUPPORT: "Not Supported.",
   WRITE_STAND_BY: "Write Stand by...",
 };
