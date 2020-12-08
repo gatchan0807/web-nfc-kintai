7fca3dfc99b35f9149248656dc992ca983b437ef
がっちゃん <yuki.dolphin.koga@gmail.com>
2020-12-09 08:49:44 +0900

    🤡 Mock用APIを作成（POSTの場合とGETの場合でレスポンス変えるだけ）

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  if (req.method === "POST") {
+    res.statusCode = 200;
+    res.json({ name: "John Doe" });
+  } else if (req.method === "GET") {
+    res.statusCode = 200;
+    res.json({ name: "Hello John." });
+  }
+};
